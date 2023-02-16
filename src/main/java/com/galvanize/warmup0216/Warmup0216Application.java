4752f62736c5baab1cea461bf0ac863fa9a64bf5
JamesHollis <james.hollis6@gmail.com>
2023-02-16 09:14:30 -0600

    Initial commit.

---
+++ src/main/java/com/galvanize/warmup0216/Warmup0216Application.java
@@ -0,0 +1,13 @@
+package com.galvanize.warmup0216;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Warmup0216Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Warmup0216Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/warmup0216/Warmup0216ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.warmup0216;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Warmup0216ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
